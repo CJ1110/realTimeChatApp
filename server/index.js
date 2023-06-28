a0e8fd13b891ce81604c06dc0c74e332d4f7425c
Cj-07 <98514726+Cj-07@users.noreply.github.com>
2023-06-28 08:25:36 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -9,15 +9,28 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL,{
+// mongoose
+// .connect(process.env.MONGO_URL,{
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// }).then(()=>{
+//     console.log("DB connection Successfull");
+
+// })
+// const server  = app.listen(process.env.PORT,()=> {
+//     console.log(`Server started on ${process.env.PORT}`);
+// })
+// .catch((err) => {
+//     console.log(err.message);
+//   });
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-});then(()=>{
-    console.log("DB connection Successfull");
-
-})
-const server  = app.listen(process.env.PORT,()=> {
-    console.log (`server started on port ${process.env.PORT}`);
-}).catch(err)=>   {
-
-};
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
